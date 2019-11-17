c3c4d65401a2c3201c044ad27f6473a656131b8b
Flor Campos Flores <u201601859@upc.edu.pe>
2019-11-17 11:48:07 -0500

    More exercises

---
+++ OperadoresCondicionales/10.-OperadorTernario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._OperadorTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //un operador es más eficiente que una estructura de control
+            Console.Write("\n\tNota [0,20]: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string msg = (n >= 0) && (n <= 20) //la condición del if
+                ? "\n\tNota Correcta"
+                : "\n\tNota Incorrecta";
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OperadoresCondicionales/11.-OperadorTernario02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._OperadorTernario02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask 3 values, after that print the lower, center and the greatest
+            // Para usarlo en lugar del if
+
+            Console.Write("\n\tValor 1 : ");
+            double v1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\n\tValor 2 : ");
+            double v2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\n\tValor 3 : ");
+            double v3 = Convert.ToDouble(Console.ReadLine());
+
+            double min = (v1 <= v2 && v1 <= v3) // estas son las condiciones
+                ? v1 : v2 <= v1 && v2 <= v3 // "?" se lee entonces -- primero es un NOR
+                ? v2 : v3;
+
+            double max = (v1 >= v2 && v1 >= v3) // estas son las condiciones
+                ? v1 : v2 >= v1 && v2 >= v3 // "?" se lee entonces -- primero es un NOR
+                ? v2 : v3;
+
+            double cen = (v1 + v2 + v3) - (min + max);
+
+            Console.WriteLine("\n\tEl menor es : " + min);
+            Console.WriteLine("\n\tEl mayor es : " + max);
+            Console.WriteLine("\n\tEl central es : " + cen);
+            Console.ReadKey();
+        }
+    }
+}
