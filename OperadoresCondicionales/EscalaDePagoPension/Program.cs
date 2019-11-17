e5e846793d99e76c972c9a5d10eb7e54f8203ef0
Flor Campos Flores <u201601859@upc.edu.pe>
2019-11-17 13:19:52 -0500

    Anidada : switch if (operadores ternarios)

---
+++ OperadoresCondicionales/EscalaDePagoPension/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscalaDePagoPension
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("\n\tIngrese la cantida de cursos a llevar : ");
+            int cantCursos = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n\tIngrese la categoría : ");
+            string categoria = Console.ReadLine();
+
+            double cuotaFija = 350;
+            double cuotaVariable = 0;
+
+            switch (categoria)
+            {
+                case "A":
+                    cuotaVariable = (cantCursos >= 1 && cantCursos <= 5)
+                    ? 400 : (cantCursos >= 6 && cantCursos <= 8)
+                    ? 600 : 900;
+                    break;
+                case "B":
+                    cuotaVariable = (cantCursos >= 1 && cantCursos <= 3)
+                    ? 400 : (cantCursos >= 4 && cantCursos <= 7)
+                    ? 600 : 900;
+                    break;
+                case "C":
+                    cuotaVariable = (cantCursos >= 1 && cantCursos <= 3)
+                    ? 400 : (cantCursos >= 4 && cantCursos <= 7)
+                    ? 600 : 900;
+                    break;
+                case "D":
+                    cuotaVariable = (cantCursos >= 1 && cantCursos <= 4)
+                     ? 400 : (cantCursos >= 5 && cantCursos <= 8)
+                    ? 600 : 900;
+                    break;
+                default:
+                    Console.WriteLine("\n\tCategoría Errónea");
+                    break;
+            }
+
+            double importeCuota = cuotaFija + cuotaVariable;
+
+            Console.WriteLine("\n\tEl monto de la boleta es : " + importeCuota);
+            Console.ReadKey();
+        }
+
+    }
+          
+    
+}
